473d1313c9d1d70dde8b639ee90fb63267b2c118
farhatahmad <ahmad.af.farhat@gmail.com>
2020-03-17 16:49:55 -0400

    BBBSL-48: Added custom health checks
    BBBSL-48: Minor refactoring
    
    Tweaks to nginx template
    
    Added dotfile example
    
    generalized gitlab-ci script for publishing
    
    Extended dotenv example

---
+++ app/controllers/health_check_controller.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+class HealthCheckController < ApplicationController
+  def all
+    @cache_expire = 10.seconds
+
+    begin
+      cache_check
+      database_check
+    rescue StandardError => e
+      render(plain: "Health Check Failure: #{e}")
+    end
+
+    render(plain: 'success')
+  end
+
+  private
+
+  def cache_check
+    raise 'Unable to write to cache' unless Rails.cache.write('__health_check_cache_write__', 'true', expires_in: @cache_expire)
+    raise 'Unable to read from cache' unless Rails.cache.read('__health_check_cache_write__', 'true', expires_in: @cache_expire)
+  end
+
+  def database_check
+    if defined?(ActiveRecord)
+      raise 'Database not responding' unless ActiveRecord::Migrator.current_version
+    end
+    raise 'Pending migrations' unless ActiveRecord::Migration.check_pending!.nil?
+  end
+end
