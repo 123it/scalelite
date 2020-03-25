24609eecc54fa854cd2519d005718a90254a7fc6
jfederico <jesus@123it.ca>
2020-03-25 19:52:24 -0400

    Forced response to 200 in healt_check

--- app/controllers/health_check_controller.rb
+++ app/controllers/health_check_controller.rb
@@ -11,7 +11,7 @@ class HealthCheckController < ApplicationController
       render(plain: "Health Check Failure: #{e}")
     end
 
-    head(:ok)
+    render(status: 200, 'success')
   end
 
   private
